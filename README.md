b4eaad209e2b15709199ad7efd629d8d92c8becc
Peter <ps.in.nz@gmail.com>
2018-03-26 14:29:17 +1300

    app now functional

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weather-app-1
 Build a simple Weather App with Node.js in just 16 lines of code
+
+[**Reference:**](https://codeburst.io/build-a-simple-weather-app-with-node-js-in-just-16-lines-of-code-32261690901d)
+
+*Learn to make API calls and build a Command Line Weather App in just 15 minutes.*
---
+++ index.js
@@ -0,0 +1,24 @@
+const request = require('request')
+const argv = require('yargs').argv
+
+let link = 'http://api.openweathermap.org/data/2.5/weather?q='
+let apiKey = 'f5d21c449bf00917e5121a2371ff8adc'
+let city = argv.c || 'whangarei'
+let country = 'nz'
+let units = 'metric' // metric or imperial
+let url = `${link}${city},${country}&units=${units}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log('error:', error)
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp}\u00B0C in ${weather.name}!`
+    console.log(message)
+  }
+})
+
+// usage:
+// node index gives current temperature in Whangarei
+// node index -c wellington gives temperature in another NZ town/city
+// node index -c 'palmerston north'
